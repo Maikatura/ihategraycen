4de960a6cc374a561ff155cb6462c1050af609f9
Maikatura <31046190+Maikatura@users.noreply.github.com>
2024-04-13 15:33:32 +0200

    Updated to use php for routing

---
+++ README.md
@@ -0,0 +1,2 @@
+The server uses php. So please dont use any malicious stuff and submit it.
+If you do I will report it and you will be blacklisted from ever contributing again.
--- index.php
+++ index.php
@@ -1,68 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>ihategraycen</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="i hate graycen">
-    <meta name="keywords" content="graycen, hate, twitch">
-    <meta property="og:image" content="https://ihategraycen.com/static/1.png">
-  
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <style>
-        body {
-            background-color: #000000;
-        }
-        #localVideo {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            display: none;
-        }
-    </style>
-</head>
-<body>
-    <div class="d-flex justify-content-center align-items-center vh-100 flex-column">
-        <img src="static/1.png" class="mb-3" alt="Your Image"><br>
-        <div class="d-flex">
-            <a href="https://www.twitch.tv/graycen" id="twitchLink" class="btn btn-primary mr-2" target="_blank">
-                <i class="fab fa-twitch"></i> Twitch
-            </a>
-            <a href="https://www.linkedin.com/in/grayadams1/" id="linkedInLink" class="btn btn-primary" target="_blank">
-                <i class="fab fa-linkedin-in"></i> LinkedIn
-            </a>
-        </div>
-        
+<?php
+
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', ['HomeController', 'index']);
+
+   });
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        header("HTTP/1.0 404 Not Found");
+        echo '404 Not Found';
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        header("HTTP/1.0 405 Method Not Allowed");
+        echo '405 Method Not Allowed';
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+
+        [$controller, $method] = $handler;
+
     
-    </div>
+        require __DIR__ . '/Controllers/' . $controller . '.php';
+        
+        $classController = new $controller();
+        $classController->$method($vars);
 
-    <video id="localVideo" width="100%">
-        <source src="static/3.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
 
-    <script>
-        document.getElementById('twitchLink').addEventListener('click', function(e) {
-            e.preventDefault();
-            var video = document.getElementById('localVideo');
-            video.style.display = 'block';
-            video.play();
-            setTimeout(function() {
-                window.location.href = e.target.href;
-            }, 2000); // Redirect after 1.5 seconds
-        });
-
-        document.getElementById('linkedInLink').addEventListener('click', function(e) {
-            e.preventDefault();
-            var video = document.getElementById('localVideo');
-            video.style.display = 'block';
-            video.play();
-            setTimeout(function() {
-                window.location.href = e.target.href;
-            }, 2000); // Redirect after 1.5 seconds
-        });
-    </script>
-</body>
-</html>
+        break;
+    default:
+        header("HTTP/1.0 500 Internal Server Error");
+        echo '500 Internal Server Error';
+}
+?>
